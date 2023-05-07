a24751c7a24c4a275ad6ea010a0dde704500e662
Adam Bagrianow <bagrianow@gmail.com>
2023-05-07 15:14:53 +0200

    Detail page about exercises is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
-import HeroBanner from "../components/HeroBanner";
-import SearchExercises from "../components/SearchExercises";
+
 import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  const [bodyPart, setbodyPart] = useState("all");
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <Box>
       <HeroBanner />
       <SearchExercises
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setbodyPart}
+        setBodyPart={setBodyPart}
       />
       <Exercises
-        exercises={exercises}
         setExercises={setExercises}
+        exercises={exercises}
         bodyPart={bodyPart}
       />
     </Box>
